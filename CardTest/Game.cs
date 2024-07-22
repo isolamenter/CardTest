c8c6a6c1df4c4411dd59de74e31737577ea316a8
yanghai <yanghai@xunlei.com>
2024-07-22 20:53:31 +0800

    update

--- CardTest/Game.cs
+++ CardTest/Game.cs
@@ -4,26 +4,27 @@ using static CardDefinition;
 public class Game
 {
     public Queue<Card> Deck;
-    public List<Card> DiscardPile;
+    public Stack<Card> DiscardPile;
     public List<List<Card>> Players;
     private CardDefinition _cardDefinition;
-
+    
     public Game(int num)
     {
         Players = new List<List<Card>>();
+        _cardDefinition = new CardDefinition();
+        
         for (var i = 0; i < num; i++)
         {
             var player = new List<Card>();
             Players.Add(player);
         }
-
-        _cardDefinition = new CardDefinition();
     }
 
     public void Start()
     {
         var cards = _cardDefinition.GetNewCards();
         Deck = new Queue<Card>(_cardDefinition.ShuffleCards(cards));
+        DiscardPile = new Stack<Card>();
     }
 
     public void Deal()
@@ -36,6 +37,56 @@ public class Game
             }
         }
     }
+
+    public void PlayerDrawFromDeck()
+    {
+        var card = DrawFromDeck();
+        
+    }
+
+    public void PlayerDrawFromDiscardPile()
+    {
+        
+    }
+    
+    public void PlayerCallCABO()
+    {
+        
+    }
+
+    public void DiscardCards(List<Card> chosenCards)
+    {
+        foreach (var card in chosenCards)
+        {
+            DiscardPile.Push(card);
+        }
+    }
+
+    public void ExchangeCards(List<Card> playerCards, List<Card> chosenCards, Card drawnCard)
+    {
+        foreach (var card in chosenCards)
+        {
+            playerCards.Remove(card);
+        }
+        DiscardCards(chosenCards);
+        playerCards.Add(drawnCard);
+    }
+
+    public void Peek(List<Card> playerCards, int index)
+    {
+        
+    }
+
+    public void Spy(List<Card> playerCards, int index)
+    {
+        
+    }
+
+    public void Swap(List<Card> player1Cards, int index1, List<Card> player2Cards, int index2)
+    {
+        
+    }
+    
     
     private Card DrawFromDeck()
     {
@@ -46,6 +97,11 @@ public class Game
 
         NewDeck();
         return Deck.Dequeue();
+    }
+
+    private Card DrawFromDiscardPile()
+    {
+        return DiscardPile.Pop();
     }
     
     private void NewDeck()
